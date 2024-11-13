58c59158527386e869b352eb974eb9e0a0a07393
GayerLeonet <gabrielleonett@gmail.com>
2024-11-13 17:47:37 -0400

    Segundo commit ya empiezo a entender laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/about.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <h1>Acerca de Nosotros</h1>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/contact.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Contacto</h1>
+</body>
+</html>
